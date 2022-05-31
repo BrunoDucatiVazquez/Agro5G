5a31a269278f9a941ce9f63d4d7bd6d13e388680
BrunoVazquez <86581876+BrunoDucatiVazquez@users.noreply.github.com>
2022-05-31 13:04:09 -0300

    Add files via upload

---
+++ 5GPy-master/Agro5G.py
@@ -0,0 +1,44 @@
+from PyQt5 import uic,QtWidgets
+import plotly
+import plotly.express as px
+import pandas as pd
+
+def openVisualizationFile():
+    telaInicial.close()
+    Visualization.show()
+
+def backMenu():
+    Visualization.close()
+    telaInicial.show()
+
+def executeAlgorithmAndVisualizateData(self):
+    while True:
+        import simulationFinal
+        Visualization.lineEdit.setText(str(simulationFinal.soil_sensor_Temperature.setTemperature))
+        Visualization.lineEdit_2.setText(str(simulationFinal.sensor_Humidity.setHumidity))
+        Visualization.lineEdit_3.setText(str(simulationFinal.sensor_Ph.setPh))
+        break
+    
+def showGraphic():
+    excel_file = 'teste.xlsx'
+    df = pd.read_excel(excel_file)
+    fig = px.scatter(df, x="Temperatura", y="Duração Da Agua")
+    fig2 = px.scatter(df, x="Humidade", y="Duração Da Agua")
+    fig3 = px.scatter(df, x="Ph", y="Duração Da Agua")
+    plotly.offline.plot(fig, filename="temperature.html")
+    plotly.offline.plot(fig2, filename="Humidade.html")
+    plotly.offline.plot(fig3, filename="Ph.html")
+
+
+app = QtWidgets.QApplication([])
+
+telaInicial = uic.loadUi("TelaInicial.ui")
+Visualization = uic.loadUi("Visualization.ui")
+
+telaInicial.pushButton.clicked.connect(executeAlgorithmAndVisualizateData) #execute the irrigation Algorithm
+telaInicial.pushButton_2.clicked.connect(openVisualizationFile) # showing the values of sensors
+Visualization.pushButton.clicked.connect(backMenu) # back to the main Menu
+telaInicial.pushButton_3.clicked.connect(showGraphic) # show graphics 
+
+telaInicial.show()
+app.exec()
